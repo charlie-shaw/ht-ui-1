c6486d71c7e8b6ca4e2bf62dad59f24f589ef6ce
Charlie <email@email@example.com>
2023-01-14 18:22:27 +0800

    初始化配置

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <Charlie/>
+</template>
+
+<script>
+import Charlie from './components/Charlie.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Charlie
+  }
+}
+</script>
---
+++ src/components/Charlie.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>这是我的第一个组件</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
